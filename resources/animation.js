075b7dae77b359c28177014983491b59c8058e70
Peter Hastie <phastie@bleacherreport.com>
2017-03-12 20:50:02 -0700

    Finished and animated Flow section.

--- resources/animation.js
+++ resources/animation.js
@@ -12,6 +12,10 @@ Reveal.addEventListener('slidechanged', function(event){
     animateNoBackpressure();
   } else if (event.indexh == 5) {
     animateBackpressure();
+  } else if (event.indexh == 28) {
+    animateFlow();
+  } else if (event.indexh == 34) {
+    animateFlowPartition();
   }
 });
 
@@ -51,6 +55,102 @@ function animateBackpressure() {
       hideAfter(m1, 9000 + (10000 * i));
     }
   }
+}
+
+function animateFlow() {
+  console.log("animateFlow");
+  var s = Snap("#flow-diagram");
+
+  var fileStreamArrow = s.select("#fileStreamArrow");
+  var fromEnumerable = s.select("#fromEnumerable");
+  var producerLabel = s.select("#producerLabel");
+  var mapArrow1 = s.select("#mapArrow1");
+  var mapArrow2 = s.select("#mapArrow2");
+  var mapArrow3 = s.select("#mapArrow3");
+  var flatmap1 = s.select("#flatmap1");
+  var flatmap2 = s.select("#flatmap2");
+  var flatmap3 = s.select("#flatmap3");
+  var producerConsumerLabel = s.select("#producerConsumerLabel");
+  var reduceArrow = s.select("#reduceArrow");
+  var reduce1 = s.select("#reduce1");
+  var reduce2 = s.select("#reduce2");
+  var reduce3 = s.select("#reduce3");
+  var consumerLabel = s.select("#consumerLabel");
+
+  fileStreamArrow.attr({opacity: 0});
+  fromEnumerable.attr({opacity: 0});
+  producerLabel.attr({opacity: 0});
+  mapArrow1.attr({opacity: 0});
+  mapArrow2.attr({opacity: 0});
+  mapArrow3.attr({opacity: 0});
+  flatmap1.attr({opacity: 0});
+  flatmap2.attr({opacity: 0});
+  flatmap3.attr({opacity: 0});
+  producerConsumerLabel.attr({opacity: 0});
+  reduceArrow.attr({opacity: 0});
+  reduce1.attr({opacity: 0});
+  reduce2.attr({opacity: 0});
+  reduce3.attr({opacity: 0});
+  consumerLabel.attr({opacity: 0});
+
+  showAfter(fileStreamArrow, 2000);
+  showAfter(fromEnumerable, 2000);
+  showAfter(mapArrow1, 5000);
+  showAfter(mapArrow2, 5000);
+  showAfter(mapArrow3, 5000);
+  showAfter(flatmap1, 5000);
+  showAfter(flatmap2, 5000);
+  showAfter(flatmap3, 5000);
+  showAfter(reduceArrow, 10000);
+  showAfter(reduce1, 10000);
+  showAfter(reduce2, 10000);
+  showAfter(reduce3, 10000);
+  showAfter(producerLabel, 15000);
+  showAfter(producerConsumerLabel, 17000);
+  showAfter(consumerLabel, 19000);
+}
+
+function animateFlowPartition() {
+  console.log("animateFlowPartition");
+  var s = Snap("#know-your-data");
+
+  var arrow1 = s.select("#arrow1");
+  var arrow2 = s.select("#arrow2");
+  var arrow3 = s.select("#arrow3");
+  var mappers = s.select("#mappers");
+  var mapArrow = s.select("#mapArrow");
+  var reduce1 = s.select("#reduce1");
+  var reduce2 = s.select("#reduce2");
+  var reduce3 = s.select("#reduce3");
+  var reduceArrow = s.select("#reduceArrow");
+  var output = s.select("#output");
+
+  arrow1.attr({opacity: 0});
+  arrow2.attr({opacity: 0});
+  arrow3.attr({opacity: 0});
+  mapArrow.attr({opacity: 0});
+  reduceArrow.attr({opacity: 0});
+  output.attr({opacity: 0});
+  reduce1.attr({fill: "#add9e4", stroke: "#74a7cb"});
+  reduce2.attr({fill: "#add9e4", stroke: "#74a7cb"})
+
+  showAfter(arrow1, 10000);
+  showAfter(arrow2, 10000);
+  showAfter(arrow3, 10000);
+  showAfter(mapArrow, 15000);
+  showAfter(reduceArrow, 20000);
+  showAfter(output, 20000);
+
+  var redTimeout1 = setTimeout(function() {
+    reduce1.attr({fill: "#de8787", stroke: "#b81729"});
+  }, 25000);
+  window.animationTimeouts.push(redTimeout1);
+
+  var redTimeout2 = setTimeout(function() {
+    reduce2.attr({fill: "#de8787", stroke: "#b81729"});
+  }, 25000);
+  window.animationTimeouts.push(redTimeout2);
+
 }
 
 function blink(element, times) {
